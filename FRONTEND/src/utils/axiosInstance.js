7ad09710c00169a3af5efd8669b34cdccf79cfbb
AbdulRehman2040 <146017169+AbdulRehman2040@users.noreply.github.com>
2025-05-20 03:27:38 +0500

    hu

--- FRONTEND/src/utils/axiosInstance.js
+++ FRONTEND/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"https://url-shortner-murex-omega.vercel.app/",
+    baseURL:"http://localhost:3000",
     timeout:10000, //10s
     withCredentials:true
 })
